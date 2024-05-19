0b83a69699213ab8affe75f514e064bdc7b9a481
V1RUSnya <dima.nesed@gmail.com>
2024-05-19 09:39:24 +0300

    fix python

--- python-app/app.py
+++ python-app/app.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']db = SQLAlchemy()
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
