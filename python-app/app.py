f7b95d54e4f0e5b5f0aa15388b9adaf94c4a4b4f
V1RUSnya <dima.nesed@gmail.com>
2024-05-21 02:04:02 +0300

    Новая фича - время сообщения (только по москве)

--- python-app/app.py
+++ python-app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
+from sqlalchemy import func
+import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -9,14 +12,19 @@ db = SQLAlchemy(app)
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
 
     def __repr__(self):
         return f'<Message {self.id}>'
 
 @app.route('/')
 def index():
-    messages = Message.query.all()
-    return render_template('index.html', messages=messages)
+    messages = db.session.query(Message).all()
+    formatted_messages = []
+    for message in messages:
+        moscow_time = message.created_at + datetime.timedelta(hours=3)
+        formatted_messages.append((message, moscow_time.strftime('%H:%M:%S %d.%m.%Y')))
+    return render_template('index.html', messages=formatted_messages)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_message():
@@ -31,4 +39,3 @@ def create_message():
 if __name__ == '__main__':
     db.create_all()
     app.run(host='0.0.0.0', port=5000)
-    
